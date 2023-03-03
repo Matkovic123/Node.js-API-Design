af7384059ea0ff7d8cef03909532ade4c22b50cb
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-03 15:49:59 +0100

    Creating a JWT

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'; // load all env vars into process.env
+dotenv.config(); // you want this at the very entry to the server
+
 import app from './server';
 
 
---
+++ src/modules/auth.ts
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  ); // JWT_SECRET is basically salt
+  return token;
+};
