4958327e046198cb0fe1e2d356f745e41e05e08f
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-03 11:21:57 +0100

    server with express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-const http = require('http');
+const app = require('./server');
 
 
-const server = http.createServer(async(req,res) => {
-    if(req.method === 'GET' && req.url === '/') {
-        res.end();
-    }
-});
 
-
-server.listen(3001, () => {
-    console.log("server on http://localhost:3001");
+app.listen(3001, () => {
+    console.log('listening on http://localhost:3001');
 })
---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("hello from express");
+    res.status(200);
+    res.json({messge: "hello"})
+})
+
+
+module.exports = app;
